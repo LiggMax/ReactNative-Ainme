324b7d96f19ec4aa34da014f001c9a248f64f810
LiggPlus <wenzhouli06@gmail.com>
2025-06-24 19:03:25 +0800

    refactor(types): 添加导航类型定义
    - 新增 RootStackParamList 类型，定义根导航参数- 添加 MainScreenProps 和 AnimeDetailScreenProps 类型，用于屏幕组件
    - 声明全局导航参数类型，以便在项目中使用

---
+++ src/types/navigation.ts
@@ -0,0 +1,21 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+// 定义根导航参数类型
+export type RootStackParamList = {
+  Main: undefined;
+  AnimeDetail: {
+    id: number;
+    title?: string;
+  };
+};
+
+// 定义屏幕组件的Props类型
+export type MainScreenProps = NativeStackScreenProps<RootStackParamList, 'Main'>;
+export type AnimeDetailScreenProps = NativeStackScreenProps<RootStackParamList, 'AnimeDetail'>;
+
+// 导航相关的类型声明
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+} 
