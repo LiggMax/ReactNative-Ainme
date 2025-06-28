11460dc4771e38659c1d751e2881099103a774a4
LiggPlus <wenzhouli06@gmail.com>
2025-06-28 09:52:05 +0800

    add react-native-windows mode

---
+++ windows/Anime/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Anime::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Anime::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
